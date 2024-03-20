38270cd181193202b4d301276bbd2767c80744f3
saicharanreddychinthareddy <Cherrycharan9888@gmail.com>
2024-03-20 08:48:49 -0500

    added login and register api endpoints

---
+++ backend/endpoints/authEndpoints.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+
+const Admin = require("../models/admin");
+const Customer = require("../models/customer");
+
+
+router.post("/register-admin", async(req, res) => {
+    try{
+        const {email, password,username} = req.body;
+        const isEmailExist = await Admin.findOne({email:email});
+        if(isEmailExist){
+            return res.status(400).json({message:"Email already exists"});
+        }
+        if(password.length < 5){
+            return res.status(400).json({message:"Password must be at least 6 characters long"});
+        }
+        const admin = new Admin({
+            email: email,
+            password: password,
+            username
+        })
+        await admin.save();
+        return res.status(200).json({message:"Admin registered successfully",user:admin});
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"Internal server error"});
+    }
+});
+
+router.post("/login-admin", async(req, res) => {
+    try{
+        const {email, password} = req.body;
+        const admin = await Admin.findOne({email:email}).lean();
+        if(!admin){
+            return res.status(400).json({message:"Email not found"});
+        }
+        if(password !== admin.password){
+            return res.status(400).json({message:"Password is incorrect"});
+        }
+        return res.status(200).json({message:"Admin logged in successfully",user:admin});
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:"Internal server error"});
+    }
+})
+
+module.exports = router;
---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express=require("express");
+const app=express();
+
+const dotenv = require("dotenv");
+dotenv.config({ path: __dirname+'/.env' });
+console.log(process.env.AWS_BUCKET_NAME);
+
+const mongoose = require("mongoose");
+const cors=require("cors");
+app.use(cors());
+
+mongoose.connect("mongodb+srv://user:1234567890@cluster0.gla6bka.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db=mongoose.connection;
+db.on("error",(error)=> console.log(error));
+db.once("open",()=>console.log("connected to database"));
+
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ 
+    extended: true 
+}));
+
+const authRoutes = require("./endpoints/authEndpoints");
+app.use("/auth", authRoutes);
+
+// const adminRoutes = require("./endpoints/admin");
+// app.use("/admin", adminRoutes);
+
+
+app.all("/*",(req,res)=>{
+    return res.status(404).json({message:"Page not found"});
+});
+
+//port listening at in the server
+app.listen(3002, ()=>{
+    console.log("running on port " + 3001);
+});
