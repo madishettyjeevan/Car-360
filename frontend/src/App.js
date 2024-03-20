a10471f878e8237d78cafe1e4a38e64fe9238fa7
jmadishetty--github <123554580+madishettyjeevan@users.noreply.github.com>
2024-03-20 08:55:41 -0500

    created dashbaord page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 import {  Routes, Route } from 'react-router-dom';
 
 import Signupp from './components/signup/signup';
-// import Login from './components/login/login';
-// import Dashboard from './components/dashboard/dashboard';
+import Login from './components/login/login';
+import Dashboard from './components/dashboard/dashboard';
 // import AddCar from './components/addCar/addCar';
 // import ViewCars from './components/viewCar/viewCar';
 
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Login />} /> */}
+      <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signupp />} />
-      {/* <Route path="/dashboard/:userName" element={<Dashboard />} />
-      <Route path="/add-car/:userName" element={<AddCar />} />
+      <Route path="/dashboard/:userName" element={<Dashboard />} />
+      {/* <Route path="/add-car/:userName" element={<AddCar />} />
       <Route path="/view-cars/:userName" element={<ViewCars />} /> */}
     </Routes>
   );
