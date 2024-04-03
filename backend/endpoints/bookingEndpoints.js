bdf676ee837e1b1e38398dbad35d89b151fbe264
Deekshitha Annreddy <deekshitha2508@gmail.com>
2024-04-02 23:29:04 -0500

    created the  car booking endpoint

---
+++ backend/endpoints/bookingEndpoints.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const router = express.Router();
+const { default: mongoose } = require("mongoose");
+
+const Booking = require("../models/Bookings");
+const Car = require("../models/Cars");
+router.post("/book/:userId/:carId", async(req, res)=>{
+    try{
+        const { userId, carId } = req.params;
+        const { hours } = req.body;
+
+        console.log(hours);
+
+        if(!userId || !carId || !hours){
+            return res.status(400).json({message:"Data is invalid"});
+        }
+
+        const carToBeBooked = await Car.findOne({_id: new mongoose.Types.ObjectId(carId)});
+        if(carToBeBooked === null || carToBeBooked.currentlyBooked){
+            return res.status(400).json({message:"Sorry, Car is not available"});
+        }
+
+        const startDate = new Date();
+        const endDate = new Date(new Date().getTime() + hours * 3600000 );
+
+        const totalTime = new Date(endDate) - new Date(startDate);
+        if(totalTime/3600000 != hours){
+            console.log("something is wrong");
+        }
+        const newBooking = new Booking({
+            car:carId,
+            user:userId,
+            startDate,
+            endDate,
+            totalPrice: hours * carToBeBooked.price
+        });
+        await newBooking.save();
+
+        carToBeBooked.currentlyBooked = true;
+        await carToBeBooked.save();
+
+        res.status(200).json({message: "Car has been booked successfully"});
+    }catch(error){
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong..."});
+    }
+});
