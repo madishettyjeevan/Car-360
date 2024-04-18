a43e9e3d1bfe715e00012f2f706838b0669acd57
jmadishetty--github <123554580+madishettyjeevan@users.noreply.github.com>
2024-04-18 01:31:45 -0500

    added renew car api

--- backend/endpoints/bookingEndpoints.js
+++ backend/endpoints/bookingEndpoints.js
@@ -92,38 +92,42 @@ router.get("/get-cars/only/:userId", async (req, res) => {
     }
 });
 
-router.get("/renewe-ars/only/:userId", async (req, res) => {
-    try {
-        const userId = req.params.userId;
-        const carDetails = [];
-        const cars = await Car.find({ owner: new mongoose.Types.ObjectId(userId) });
-        for(const element of cars){
-            let details = {
-                brand: element.brand,
-                description: element.description,
-                imageUrl: element.imageUrl,
-                price: element.price,
-                color: element.color,
-                year: element.year,
-                model: element.model,
-                currentlyBooked: element.currentlyBooked
-            };
-            if(element.currentlyBooked){
-                const bookingDetails = await Booking.findOne({car: element._id, bookingActive: true}).populate('user');
-                details = {
-                    ...details,
-                    bookedBy: bookingDetails.user.username,
-                    bookingStart: bookingDetails.startDate,
-                    bookingEnd: bookingDetails.endDate,
-                    totalPrice: bookingDetails.totalPrice
-                }
-            }
-            carDetails.push(details);
+router.post("/renew/:userId/:carId", async(req, res) => {
+    try{
+        const { userId, carId } = req.params;
+        const { hours } = req.body;
+
+        if(!userId || !carId || !hours){
+            return res.status(400).json({message:"Data is invalid"});
         }
-        return res.status(200).json(carDetails);
-    } catch (error) {
+
+        const bookingToBeRenewed = await Booking.findOne({bookingActive: true, car: new mongoose.Types.ObjectId(carId), user: new mongoose.Types.ObjectId(userId)});
+        if(bookingToBeRenewed === null){
+            return res.status(400).json({message:"Sorry, Car is not available"});
+        }
+
+        const { username, email } = await User.findOne({_id: new mongoose.Types.ObjectId(userId)}, {_id:0});
+        const { price, brand, model } = await Car.findOne({_id:new mongoose.Types.ObjectId(carId)},{_id:0});
+        const startDate = new Date(bookingToBeRenewed.endDate);
+        const endDate = new Date(startDate.getTime() + hours * 3600000 );
+        const totalPrice = bookingToBeRenewed.totalPrice + price*hours;
+
+        bookingToBeRenewed.endDate = endDate;
+        bookingToBeRenewed.totalPrice = totalPrice;
+        bookingToBeRenewed.bookingTBENotification = false;
+
+        await sendEmail(
+            username,
+            email,
+            'Car Booking Renewel',
+            `\n\nYour booking for car ${brand} ${model} has been renewed. New End time is ${endDate.toLocaleString()}`
+        )
+        await bookingToBeRenewed.save();
+        res.status(200).json({message: "Booking has been renewed successfully"});
+    }catch(error){
         console.log(error);
         res.status(500).json({ message: "Something went wrong..."});
     }
 });
+
 
