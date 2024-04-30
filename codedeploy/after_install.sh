a9d3e60a0b4847fa3fadb1d6649929194361cc6c
madishettyjeevan <123554580+madishettyjeevan@users.noreply.github.com>
2024-04-30 03:29:00 +0000

    added deploy scripts

---
+++ codedeploy/after_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# run backend
+cd /home/ubuntu/se-project/backend/ && sudo npm install
+# run frontend
+cd /home/ubuntu/se-project/frontend/ && sudo npm install
--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,12 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3002" 
+
+const publicIP = '<public-ip>' // elastic IP of development server
+
+const BASE_URL = `http://${publicIP}:3002`
 
 const axiosInstance=axios.create({
     baseURL:BASE_URL,
 });
 
 export default axiosInstance; 
+
