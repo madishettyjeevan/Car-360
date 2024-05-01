95b5800facc31034d9b3ce8aea19508c432b78d9
saicharanreddychinthareddy <161169496+saicharanreddychinthareddy@users.noreply.github.com>
2024-05-01 10:17:52 +0530

    resolved a bug in axios folder made it global for whole application

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -4,9 +4,11 @@ const publicIP = '<public-ip>' // elastic IP of development server
 
 const BASE_URL = `http://${publicIP}:3002`
 
+
 const axiosInstance=axios.create({
     baseURL:BASE_URL,
 });
+
 
 export default axiosInstance; 
 
