8438f0226e1c4e024ec781743ce9bbb73dbde566
Rukmini Devi Y <161406311+rukminisharma0108@users.noreply.github.com>
2024-05-01 10:28:55 +0530

    Updated app_start.sh script for the continues deployment for starting the application

--- codedeploy/app_start.sh
+++ codedeploy/app_start.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 # stop all server
 sudo pm2 stop all
 sudo pm2 delete all
 
 ## change the ip
 sudo sed -i "s/<public-ip>/$(curl -s http://checkip.amazonaws.com)/g" /home/ubuntu/se-project/frontend/src/axios.js
-
 
 # run backend
 cd /home/ubuntu/se-project/backend/ && sudo npm run deploy
